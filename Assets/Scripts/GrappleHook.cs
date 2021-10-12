b1d1190bb43de6b998dca08378e60bc002d3807d
angelobrajuka <brajuka.angelo@gmail.com>
2021-10-11 22:35:22 -0400

    pretty solid single joint grapple hook, no cable renderer yet

--- Assets/Scripts/GrappleHook.cs
+++ Assets/Scripts/GrappleHook.cs
@@ -2,14 +2,46 @@ using UnityEngine;
 
 public class GrappleHook : MonoBehaviour
 {
+    // hierarchy
+    public ConfigurableJoint configJoint;
+    public SpringJoint springJoint;
+    public Rigidbody m_rigidbody;
+
+    public FixedJoint fixedJoint;
     public ThreeDM threeDM;
+
+    public void SetJointMotion(bool active)
+    {
+        ConfigurableJointMotion motionType = active ? ConfigurableJointMotion.Limited : ConfigurableJointMotion.Free;
+        configJoint.xMotion = motionType;
+        configJoint.yMotion = motionType;
+        configJoint.zMotion = motionType;
+    }
+
+    public void LockMotion(Rigidbody other)
+    {
+        fixedJoint = gameObject.AddComponent<FixedJoint>();
+        fixedJoint.connectedBody = other;
+        SetJointMotion(true);
+    }
+
+    public void Retract()
+    {
+        if(fixedJoint != null && fixedJoint.connectedBody == null) Destroy(fixedJoint);
+
+        SetJointMotion(false);
+
+        springJoint.spring = threeDM.autoRetractSpringForce;
+
+        enabled = false;
+    }
 
     void OnCollisionEnter(Collision other)
     {
         if(!enabled) return;
 
-        threeDM.SetJointMotion(ConfigurableJointMotion.Limited);
-        GetComponent<Rigidbody>().isKinematic = true;
+        LockMotion(other.rigidbody);
+
         enabled = false;
     }
 }
--- Assets/Scripts/ThreeDM.cs
+++ Assets/Scripts/ThreeDM.cs
@@ -5,31 +5,16 @@ public class ThreeDM : MonoBehaviour
 {
     // hierarchy
     public GameObject prefab_hook;
-    public GameObject prefab_cableSegment;
-    public ConfigurableJoint joint;
     public float shootSpeed;
     public float maxAdjustSpeed;
-    public float autoRetractSpeed;
+    public float autoRetractSpringForce;
 
     GrappleHook hook;
-    Stack<GameObject> cableSegments = new Stack<GameObject>();
-    bool autoRetracting;
     float targetLength=0, length=0;
-
-    public void SetJointMotion(ConfigurableJointMotion motionType)
-    {
-        joint.xMotion = motionType;
-        joint.yMotion = motionType;
-        joint.zMotion = motionType;
-    }
 
     public void DestroyHook()
     {
         Destroy(hook.gameObject);
-        while(cableSegments.Count > 0)
-        {
-            Destroy(cableSegments.Pop());
-        }
         hook = null;
     }
 
@@ -40,16 +25,23 @@ public class ThreeDM : MonoBehaviour
             var obj = Instantiate(prefab_hook, transform.position, PlayerMovement.instance.t_camera.rotation, null);
             hook = obj.GetComponent<GrappleHook>();
             hook.threeDM = this;
+            hook.configJoint.connectedBody = PlayerMovement.m_rigidbody;
+            hook.springJoint.connectedBody = PlayerMovement.m_rigidbody;
             var rb = obj.GetComponent<Rigidbody>();
             rb.AddRelativeForce(Vector3.forward*shootSpeed);
-            autoRetracting = false;
             targetLength = 0;
         }
         else
         {
-            hook.enabled = false;
-            autoRetracting = true;
-            hook.GetComponent<Rigidbody>().isKinematic = false;
+            hook.Retract();
+            // var ll = hook.configJoint.linearLimit;
+            // ll.contactDistance = 0;
+            // ll.limit = 0;
+            // ll.bounciness = 0;
+            // hook.configJoint.linearLimit = ll;
+            // var lls = hook.configJoint.linearLimitSpring;
+            // lls.spring = autoRetractSpringForce;
+            // hook.configJoint.linearLimitSpring = lls;
         }
     }
 
@@ -57,48 +49,44 @@ public class ThreeDM : MonoBehaviour
     {
         if(hook == null) return;
 
-        targetLength += amount;
-        while(targetLength - length > 1)
-        {
-            length += 1;
-            var oldLast = cableSegments.Count == 0 ? hook.gameObject : cableSegments.Peek();
-            var newLast = Instantiate(prefab_cableSegment, transform.position, PlayerMovement.instance.t_camera.rotation, null);
-            cableSegments.Push(newLast);
-            var newJoint = newLast.GetComponent<ConfigurableJoint>();
-            newJoint.connectedBody = oldLast.GetComponent<Rigidbody>();
-        }
-        while(targetLength - length < 1)
-        {
-            length -= 1;
-            if(cableSegments.Count > 0)
-            {
-                Destroy(cableSegments.Pop());
-                if(cableSegments.Count <= 1)
-                {
-                    DestroyHook();
-                }
-                else
-                {
-                    cableSegments.Peek().GetComponent<ConfigurableJoint>().connectedBody = PlayerMovement.m_rigidbody;
-                }
-            }
-            else
-            {
-            }
-        }
-        if(cableSegments.Count > 0)
-            joint.connectedBody = cableSegments.Peek().GetComponent<Rigidbody>();
+        // targetLength += amount;
+        // while(targetLength - length > 1)
+        // {
+        //     length += 1;
+        //     var oldLast = cableSegments.Count == 0 ? hook.gameObject : cableSegments.Peek();
+        //     var newLast = Instantiate(prefab_cableSegment, transform.position, PlayerMovement.instance.t_camera.rotation, null);
+        //     cableSegments.Push(newLast);
+        //     var newJoint = newLast.GetComponent<ConfigurableJoint>();
+        //     newJoint.connectedBody = oldLast.GetComponent<Rigidbody>();
+        // }
+        // while(targetLength - length < 1)
+        // {
+        //     length -= 1;
+        //     if(cableSegments.Count > 0)
+        //     {
+        //         Destroy(cableSegments.Pop());
+        //         if(cableSegments.Count <= 1)
+        //         {
+        //             DestroyHook();
+        //         }
+        //         else
+        //         {
+        //             cableSegments.Peek().GetComponent<ConfigurableJoint>().connectedBody = PlayerMovement.m_rigidbody;
+        //         }
+        //     }
+        //     else
+        //     {
+        //     }
+        // }
+        // if(cableSegments.Count > 0)
+        //     joint.connectedBody = cableSegments.Peek().GetComponent<Rigidbody>();
     }
 
     public void Update()
     {
-        if(hook != null && hook.enabled && (cableSegments.Count == 0 || Vector3.Distance(cableSegments.Peek().transform.position, transform.position) > 1))
-        {
-            AdjustDistance(1);
-        }
-        if(autoRetracting)
-        {
-            AdjustDistance(-autoRetractSpeed*Time.deltaTime);
-        }
+        // if(hook != null && hook.enabled && (cableSegments.Count == 0 || Vector3.Distance(cableSegments.Peek().transform.position, transform.position) > 1))
+        // {
+        //     AdjustDistance(1);
+        // }
     }
 }
