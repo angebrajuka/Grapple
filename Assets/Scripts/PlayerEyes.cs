76b692288160ef3977cee0f7a9a340580fcf69b6
angelobrajuka <brajuka.angelo@gmail.com>
2021-10-15 17:26:48 -0400

    easy player eye raycast

--- Assets/Scripts/PlayerEyes.cs
+++ Assets/Scripts/PlayerEyes.cs
@@ -4,12 +4,10 @@ public class PlayerEyes : MonoBehaviour
 {
     const int defaultLayerMask = ~(Layers.PLAYER_ARMS | Layers.PLAYER | Layers.GRAPPLE_HOOK);
 
-    public static bool Raycast(float range, out Transform hit, Vector3 directionOffset=default(Vector3), int layermask=defaultLayerMask)
+    public static bool Raycast(float range, out RaycastHit hit, Vector3 directionOffset=default(Vector3), int layermask=defaultLayerMask)
     {
         if(directionOffset == default(Vector3)) directionOffset = Vector3.zero;
 
-        hit = null;
-
-        return false;
+        return Physics.Raycast(PlayerMovement.instance.t_camera.position, PlayerMovement.instance.t_camera.TransformDirection(Vector3.forward), out hit, range, layermask);
     }
 }
