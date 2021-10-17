5240d6f333d430dfcc0ea7f18ba3afe214d7a993
angelobrajuka <brajuka.angelo@gmail.com>
2021-10-17 17:27:29 -0400

    shotgun model

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -8,19 +8,19 @@ public class PlayerShooting : MonoBehaviour
 
     void Update()
     {
-        var targetDirection = PlayerMovement.instance.t_camera.forward;
+        // var targetDirection = PlayerMovement.instance.t_camera.forward;
 
-        RaycastHit hit;
-        if(PlayerEyes.Raycast(out hit, Mathf.Infinity, Vector3.forward*2))
-        {
-            targetDirection = (hit.point-gunPosition.position).normalized;
-        }
+        // RaycastHit hit;
+        // if(PlayerEyes.Raycast(out hit, Mathf.Infinity, Vector3.forward*2))
+        // {
+        //     targetDirection = (hit.point-gunPosition.position).normalized;
+        // }
 
-        var newDirection = Vector3.MoveTowards(gunPosition.forward, targetDirection, moveSpeed*Time.deltaTime);
-        gunPosition.LookAt(gunPosition.position+newDirection);
-        var euler = gunPosition.localEulerAngles;
-        euler.z = 0;
-        gunPosition.localEulerAngles = euler;
+        // var newDirection = Vector3.MoveTowards(gunPosition.forward, targetDirection, moveSpeed*Time.deltaTime);
+        // gunPosition.LookAt(gunPosition.position+newDirection);
+        // var euler = gunPosition.localEulerAngles;
+        // euler.z = 0;
+        // gunPosition.localEulerAngles = euler;
     }
 
     public static bool ShootBullet(Gun gun)
