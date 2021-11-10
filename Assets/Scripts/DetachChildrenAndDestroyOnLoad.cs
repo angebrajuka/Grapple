0b36b06326e225626e8587f142586f03d680fc01
angelobrajuka <brajuka.angelo@gmail.com>
2021-11-09 21:58:48 -0500

    stuff

--- Assets/Scripts/DetachChildrenAndDestroyOnLoad.cs
+++ Assets/Scripts/DetachChildrenAndDestroyOnLoad.cs
@@ -4,10 +4,7 @@ public class DetachChildrenAndDestroyOnLoad : MonoBehaviour
 {
     void Start()
     {
-        for(int i=0; i<transform.childCount; i++)
-        {
-            transform.GetChild(0).parent = null;
-        }
+        transform.DetachChildren();
         Destroy(gameObject);
     }
 }
