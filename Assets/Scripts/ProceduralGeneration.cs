b5878e50510e8414bbe8a59967c8bb0bd9357c1c
angelobrajuka <brajuka.angelo@gmail.com>
2021-11-28 14:02:36 -0500

    full fill

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -29,7 +29,7 @@ public class ProceduralGeneration : MonoBehaviour
 
         var mesh = new Mesh();
         Vector3[] vertices = new Vector3[(int)Math.Sqr(DynamicLoading.CHUNK_SIZE+1)];
-        int[] triangles = new int[(int)Math.Sqr(DynamicLoading.CHUNK_SIZE)*3];
+        int[] triangles = new int[(int)Math.Sqr(DynamicLoading.CHUNK_SIZE)*6];
 
         int i=0;
         for(int x=0; x<=DynamicLoading.CHUNK_SIZE; ++x) for(int z=0; z<=DynamicLoading.CHUNK_SIZE; ++z)
@@ -37,9 +37,12 @@ public class ProceduralGeneration : MonoBehaviour
             vertices[(DynamicLoading.CHUNK_SIZE+1)*x+z] = new Vector3(x, Random.value/2f, z);
             if(x<DynamicLoading.CHUNK_SIZE && z<DynamicLoading.CHUNK_SIZE)
             {
-                triangles[3*i]   = (DynamicLoading.CHUNK_SIZE+1)*x+z;
-                triangles[3*i+1] = (DynamicLoading.CHUNK_SIZE+1)*x+z+1;
-                triangles[3*i+2] = (DynamicLoading.CHUNK_SIZE+1)*(x+1)+z;
+                triangles[6*i]   = (DynamicLoading.CHUNK_SIZE+1)*x+z;
+                triangles[6*i+1] = (DynamicLoading.CHUNK_SIZE+1)*x+z+1;
+                triangles[6*i+2] = (DynamicLoading.CHUNK_SIZE+1)*(x+1)+z;
+                triangles[6*i+3] = (DynamicLoading.CHUNK_SIZE+1)*(x+1)+z;
+                triangles[6*i+4] = (DynamicLoading.CHUNK_SIZE+1)*x+z+1;
+                triangles[6*i+5] = (DynamicLoading.CHUNK_SIZE+1)*(x+1)+z+1;
                 ++i;
             }
         }
