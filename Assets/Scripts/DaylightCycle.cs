72c9eeff675aeca91c0cf1b9b7801f6fa36e545b
angelobrajuka <brajuka.angelo@gmail.com>
2022-03-01 21:55:13 -0500

    fixed overflow warning fixed daylight cycle

--- Assets/Scripts/DaylightCycle.cs
+++ Assets/Scripts/DaylightCycle.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class DaylightCycle : MonoBehaviour
 {
-    public const float k_MORNING=240, k_DAY=15, k_EVENING=135, k_NIGHT=150;
+    public const float k_MORNING=240, k_DAY=40, k_EVENING=130, k_NIGHT=150;
 
     // hierarchy
+    public Transform sun;
+    public Light lightSource;
+    public Color colorDay, colorNight;
     public float brightness_day, brightness_night;
+    public float sunriseAngle;
 
     public static float time = k_DAY;
 
-    void Update()
-    {
+    void Update() {
         time += PauseHandler.paused ? 0 : Time.deltaTime;
         time %= k_MORNING;
-        transform.rotation = Quaternion.Euler(Math.Remap(time, 0, k_MORNING, 0, 180), transform.rotation.y, transform.rotation.z);
+        sun.localRotation = Quaternion.Euler(
+            time < k_NIGHT ? Math.Remap(time, 0, k_NIGHT, -sunriseAngle, 180+sunriseAngle) : 270,
+            sun.localRotation.y,
+            sun.localRotation.z
+        );
+        lightSource.color = colorDay;
+        lightSource.intensity = 
+            (time < k_DAY)      ? Math.Remap(time, 0, k_DAY, brightness_night, brightness_day) :
+            (time < k_EVENING   ? brightness_day :
+            (time < k_NIGHT     ? Math.Remap(time, k_EVENING, k_NIGHT, brightness_day, brightness_night) :
+            brightness_night));
     }
 }
